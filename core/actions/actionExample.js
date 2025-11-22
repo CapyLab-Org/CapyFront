2708e8940881a6c943416ad0d71fb2e1684a02da
kev <kim254097@gmail.com>
2025-11-21 21:16:37 -0300

    fix doc

--- core/actions/actionExample.js
+++ core/actions/actionExample.js
@@ -1,3 +1,10 @@
+/**
+ * @license MIT
+ * Copyright (c) 2025 CapyLab Studio
+ */
+
+import { apiRequest } from '../api.js';
+
 export async function actionExample() {
   const res = await apiRequest('/api/products');
   return res.json();
